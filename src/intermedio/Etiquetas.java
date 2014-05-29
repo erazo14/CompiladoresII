b9f02182399024cc79d76ee93bd897e01fcc29a7
Betzabe Ochoa <usuario@Martin.local>
2014-05-29 15:34:16 -0600

    cree clases, temporal, etiqueta y lista de etiquetas
    la clase temporal la cree, ya que creo que la vamos a necesitar para
    manejar los temporales a con respecto a su búsqueda.
    
    la clase etiqueta para hacer lo de backpatching y la lista de etiquetas
    para saber a que bloque tenemos que ir .
    
    tengo una duda nos e como estamos manejando lo de los bloques, creo que
    será necesario hacer una clase bloques

---
+++ src/intermedio/Etiquetas.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package intermedio;
+
+/**
+ *
+ * @author usuario
+ */
+public class Etiquetas {
+    public String comienzo = "";
+    public String siguiente = "";
+    public String siguiente1 = "";
+    public String id1="";
+    public String id2="";
+    public String id3="";
+    
+    
+    public Etiquetas(String c, String s, String s1, String id1, String id2, String id3){
+    this.comienzo = c;
+    this.siguiente = s;
+    this.siguiente1 = s1;
+    this.id1 = id1;
+    this.id2 = id2;
+    this.id3 = id3;
+    
+    }
+    
+    
+    
+    
+    
+    
+}
---
+++ src/intermedio/Temporal.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package intermedio;
+
+/**
+ *
+ * @author usuario
+ */
+public class Temporal {
+    public String name;
+    public boolean available;
+    public int tipo;
+    public int reference;
+    
+    
+    public Temporal(String n){
+    name = n;
+    available = true;
+   
+    }
+    
+}
