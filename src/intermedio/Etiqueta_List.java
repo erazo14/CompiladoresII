33488548b7c82e20d8e839eb921a2ad49d1ec5b5
Ricardo Erazo <erazo.14@hotmail.com>
2014-06-03 09:15:59 -0600

    Revert "lista etiquetas"
    This reverts commit d4e03343fc2e27d5ffbe4c445f0c6971482a426b.

--- src/intermedio/Etiqueta_List.java
+++ src/intermedio/Etiqueta_List.java
@@ -6,35 +6,10 @@
 
 package intermedio;
 
-import java.util.ArrayList;
-
 /**
  *
  * @author usuario
  */
 public class Etiqueta_List {
-     public  ArrayList <Etiquetas> LabelList = new ArrayList();
-
-    public Etiqueta_List() {
-    }
-    
-    public void  AddLabel(String paramc,String params , String params1 , String paramid1 , String paramid2 , String paramid3 ){
-        Etiquetas label = new Etiquetas (paramc,params,params1,paramid1, paramid2, paramid3);
-        LabelList.add(label);
-        
-    }
-    
-    public Etiquetas getEtiqueta(){
-        return LabelList.get(LabelList.size()-1);
-
-    }
-    
-    public void deletelastlabel(){
-        
-        LabelList.remove(LabelList.size()-1);
-        
-               
-    }
- 
     
 }
