ec96699677d2361812e12536d0c3c2bb03e3b9bc
Betzabe Ochoa <usuario@Kenia.local>
2014-06-13 19:23:35 -0600

    clase escritor
    clase para guardar el .asm

--- src/intermedio/CodigoFinal.java
+++ src/intermedio/CodigoFinal.java
@@ -8,6 +8,7 @@ package intermedio;
 
 import java.util.ArrayList;
 import java.util.Stack;
+import symtable.Scope;
 import symtable.Simbolo;
 import symtable.SymTable;
 
@@ -17,47 +18,44 @@ import symtable.SymTable;
  */
 public class CodigoFinal {
     SymTable Tabla;
-    CuadTable Cuadruplos;
-    Simbolo SimboloActual;
+    //CuadTable Cuadruplos;
+    Scope AmbitoActual;
     ArrayList<Texto> mensajes;
-    CuadTable tempo; // maneja la lista de los temporales
+    //CuadTable tempo; // maneja la lista de los temporales
     int cont_copia_string;
     Stack<ArrayList<String>> tempActivas;
-    ArrayList<String> escribir;
+    ArrayList<String> escribe;
 
-    public CodigoFinal(SymTable Tabla, CuadTable Cuadruplos, ArrayList<Texto> mensajes) {
+    public CodigoFinal(SymTable Tabla, /*CuadTable Cuadruplos,*/ ArrayList<Texto> mensajes) {
         this.Tabla = Tabla;
-        this.Cuadruplos = Cuadruplos;
-    //   this.SimboloActual = Tabla.getCurScope(); las clases estan static por eso no puedo depender de ellas
+        //this.Cuadruplos = Cuadruplos;
+        this.AmbitoActual = SymTable.rootScope; //las clases estan static por eso no puedo depender de ellas
         this.mensajes = mensajes;
-        tempo = new CuadTable();
+        //tempo = new CuadTable();
         this.cont_copia_string = 0;
         tempActivas = new Stack();
-        escribir = new ArrayList();
+        escribe = new ArrayList();
     }
     
     public void FinalCode(){
-        escribir.add(".data");
-        escribir.add("_true: .asciiz \"TRUE\" ");
-        escribir.add("_false: .asciiz \"FALSE\" ");
+        escribe.add(".data");
+        escribe.add("_true: .asciiz \"TRUE\" ");
+        escribe.add("_false: .asciiz \"FALSE\" ");
         for (Texto texto : mensajes) {
             if(texto.texto.startsWith("'")){
                 texto.texto = texto.texto.replaceAll("'", "\"");
             }
-            escribir.add(texto.nombre+": .asciiz "+texto.texto);
+            escribe.add(texto.nombre+": .asciiz "+texto.texto);
         }
-        escribir.add(".text");
-        escribir.add(".globl main");
+        escribe.add(".text");
+        escribe.add(".globl main");
         int tipo;
         int dir = 0;
         int t;
         String temp,temp2,temp3;
         Simbolo s;
-    }
-    
-    
-    
-
-
-
+        
+        
+        
+           }
 }
---
+++ src/intermedio/Escritor.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package intermedio;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author usuario
+ */
+public class Escritor {
+    public static void escribe(String archivo, ArrayList<String> instrucciones) {
+        
+        FileWriter fichero = null;
+        PrintWriter pw = null;
+        try
+        {
+            fichero = new FileWriter(archivo+".asm");
+            pw = new PrintWriter(fichero);
+
+                 for (int i=0;i<instrucciones.size();i++){
+                     pw.println(instrucciones.get(i));
+                 }
+            
+               
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+           try {
+           if (null != fichero)
+              fichero.close();
+           } catch (Exception e2) {
+              e2.printStackTrace();
+           }
+        }
+        
+    }
+}
